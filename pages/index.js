c7f4ed5415ece5be2eba02d49a8602ef4b4f6943
Francis Musango <francispaulmusango@gmail.com>
2021-11-02 19:44:57 +0300

    Mobile UI

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,25 @@
 import Head from "next/head";
-import {Fragment} from "react";
-import TopBar from '../components/ui/TopBar';
-import Carrier from '../components/ui/mainCarrier';
+import { Fragment } from "react";
+import TopBar from "../components/ui/TopBar";
+import Carrier from "../components/ui/mainCarrier";
 
 const Landing = () => {
   return (
     <Fragment>
       <Head>
         <title>MusicOlogy</title>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
         <meta
           name="description"
           content="This Is the First Nextjs Version"
         ></meta>
       </Head>
       <section>
-        <TopBar/>
+        <TopBar />
         <Carrier>
           <h1>Home</h1>
         </Carrier>
