d8b1379a2611f654a2c66b3c9fe94be64e1da845
Francis Musango <francispaulmusango@gmail.com>
2021-11-02 19:48:36 +0300

    Mobile UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout/Layout";
-
+import Head from "next/head";
 
 
 function MyApp({ Component, pageProps }) {
   return (
+    
     <Layout>
+       <Head>
+        <title>MusicOlogy</title>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta
+          name="description"
+          content="This is a music Application"
+        ></meta>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
