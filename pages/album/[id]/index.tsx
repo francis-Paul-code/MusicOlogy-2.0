4f9d208f9c2d01e730682ad5c95b98fce8584410
francis-Paul-code <francispaulmusango@gmail.com>
2021-12-05 20:57:33 +0300

    Build Bugs

--- pages/album/[id]/index.tsx
+++ pages/album/[id]/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Layout from "../../../components/Layout/Layout";
-const Singlealbum = (props) => {
+const Singlealbum = (props: any) => {
     const router = useRouter();
     const value = Object.keys(router.query)[0]
   console.log(value);
